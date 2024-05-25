8e767454e7bb08a8addf70b88987f3b0299703d2
fojlyR <frabbybics10@gmail.com>
2024-05-25 11:59:03 +0600

    new commit

--- Synchronization/src/Main.java
+++ Synchronization/src/Main.java
@@ -7,15 +7,31 @@ class Counter{
 }
 
 class Thread1 extends Thread{
+    Counter cc;
+    public Thread1(Counter c){
+        cc = c;
+    }
     public void run(){
-
+        for(int i = 1; i <= 1000; i++){
+            cc.increament();
+        }
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         Counter cc = new Counter();
-        cc.increament();
+        Thread1 t1 = new Thread1(cc);
+        Thread1 t2 = new Thread1(cc);
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         System.out.println(cc.count);
     }
 }
